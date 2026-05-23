0005040b7fdde9cd7b2e538143237908921a1882
kazkaz9717 <kazkaz9717@gmail.com>
2026-05-23 13:07:26 +0900

    新規登録・プロフィール編集のバリデーションエラーメッセージを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   has_many :rooms, dependent: :destroy
   has_many :reservations, dependent: :destroy
 
-  has_secure_password
   has_one_attached :icon_image
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, allow_nil: true
+
+  has_secure_password
 end
